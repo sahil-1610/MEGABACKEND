7cbab3011f03a00aa621999a15d34d3f63cd6f69
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2024-03-05 01:04:18 +0530

    utils files

--- src/index.js
+++ src/index.js
@@ -8,3 +8,15 @@ dotenv.config({
 
 
 connectDB()
+.then( () => {
+    app.on('error', (error) => {
+        console.log("ERROR", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB CONNECTION FAILED !!!",err);
+})
